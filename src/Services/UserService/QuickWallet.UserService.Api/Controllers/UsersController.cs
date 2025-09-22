43c47252b58c24ff43cba9fdaee744c8ac918813
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-09-23 00:49:26 +0300

    chore: add user service projects

---
+++ src/Services/UserService/QuickWallet.UserService.Api/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuickWallet.UserService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
